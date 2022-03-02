c183c705053687296d77744a0d5d7718249d9739
Luis <76595940+Luis-st@users.noreply.github.com>
2022-03-02 14:44:53 +0100

    add XBackpack and XOres to the dependencies via mavenLocal

---
+++ src/main/java/net/luis/xsurvive/XSurvive.java
@@ -0,0 +1,18 @@
+package net.luis.xsurvive;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(XSurvive.MOD_ID)
+public class XSurvive {
+	
+	public static final String MOD_ID = "xores";
+	public static final Logger LOGGER = LogManager.getLogger(XSurvive.class);
+	
+	public XSurvive() {
+		
+	}
+	
+}
