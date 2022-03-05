ba924183aeedd50815bca06430006682981b6ab0
Luis <76595940+Luis-st@users.noreply.github.com>
2022-03-05 20:17:47 +0100

    update dependencies

--- src/main/java/net/luis/xsurvive/XSurvive.java
+++ src/main/java/net/luis/xsurvive/XSurvive.java
@@ -3,16 +3,20 @@ package net.luis.xsurvive;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import net.luis.xsurvive.init.XSurviveEnchantments;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(XSurvive.MOD_ID)
 public class XSurvive {
 	
-	public static final String MOD_ID = "xores";
+	public static final String MOD_ID = "xsurvive";
 	public static final Logger LOGGER = LogManager.getLogger(XSurvive.class);
 	
 	public XSurvive() {
-		
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		XSurviveEnchantments.ENCHANTMENTS.register(modEventBus);
 	}
 	
 }
